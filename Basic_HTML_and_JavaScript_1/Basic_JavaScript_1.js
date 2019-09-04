a9f89b9627f464daa1382e9a9705e876e6f2f5e4
Jeremiah Davis <jeremiahd@gmail.com>
2019-09-04 09:01:51 -0700

    Step 128

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+alert("Hello World!");
+
+document.getElementById("p1").style.color = "Red";
+document.getElementById("p2").style.color = "#00ff00";
+document.getElementById("p3").style.color = "rgb(0, 0, 255)";
+
+
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+}
