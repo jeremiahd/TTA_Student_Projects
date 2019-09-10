a3214b6123d21d213490124d3bf1c17bc6f37ee6
Jeremiah Davis <jeremiahd@gmail.com>
2019-09-10 09:41:00 -0700

    Step 58

---
+++ 3_JAVASCRIPT/js/JavaScript_File.js
@@ -0,0 +1,16 @@
+var string1="string1", string2="string2", string3="string3";
+var string1 = string1.fontcolor("red");
+var string2 = string2.fontcolor("green");
+var string3 = string3.fontcolor("blue");
+
+string1 + string2; //expression example
+
+document.write(string1+"<br>");
+document.write(string2+"<br>");
+document.write(string3+"<br>");
+
+function myFirstFunction() {
+	var str = "This text is green!";
+	var result = str.fontcolor("green");
+	document.getElementById("Green_Text").innerHTML = result;
+}
