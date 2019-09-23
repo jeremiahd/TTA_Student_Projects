9969197134808357e65eedf28449a3c2c1e083a3
Jeremiah Davis <jeremiahd@gmail.com>
2019-09-23 09:08:14 -0700

    Fix pizza assignment

--- 3_JAVASCRIPT/Projects/pizza_project/Pizza_JavaScript/Pizza.js
+++ 3_JAVASCRIPT/Projects/pizza_project/Pizza_JavaScript/Pizza.js
@@ -1,71 +1,89 @@
+// Lets store our data in an object
+class Order {
+	constructor() {	
+		this.size = "";
+		this.sizeTotal = 0;
+		
+		this.meats = [];
+		this.meatTotal = 0.00;
+		
+		this.veg = [];
+		this.vegTotal = 0.00;
+	}
+	pieSize() {
+		// All size elements
+		var allSizes = document.getElementsByClassName("size");
+		
+		// Store only selected size
+		for(var i=0; i<allSizes.length; i++)
+			if(allSizes[i].checked)
+				this.size = allSizes[i].value;
+		
+		// set sizeTotal
+		switch(this.size) {
+			case "Personal Pizza":
+				this.sizeTotal = 6;
+			break;
+			case "Small Pizza":
+				this.sizeTotal = 8;
+			break;
+			case "Medium Pizza":
+				this.sizeTotal = 10;
+			break;
+			case "Large Pizza":
+				this.sizeTotal = 14;
+			break;
+			case "Extra Large Pizza":
+				this.sizeTotal = 16;
+			break;
+		}		
+	}
+	addMeats() {
+		// All meat elements
+		var allMeats = document.getElementsByClassName("meats");
+		
+		// Store only selected meat elements
+		for(var i=0; i<allMeats.length; i++)
+			if(allMeats[i].checked)
+				this.meats.push(allMeats[i].value)
+			
+		// Set meat cost
+		if(this.meats.length <= 1) this.meatTotal = 0.00;
+		else this.meatTotal = this.meats.length-1;
+	}
+	addVeggies() {
+		// All veggie elements
+		var allVeg = document.getElementsByClassName("veg");
+		
+		// Store only selected veggie elements
+		for(var i=0; i<allVeg.length; i++)
+			if(allVeg[i].checked)
+				this.veg.push(allVeg[i].value);
+		
+		// Set veg cost
+		if(this.veg.length <= 1) this.vegTotal = 0.00;
+		else this.vegTotal = this.veg.length-1;
+	}
+	total() {
+		return (this.sizeTotal + this.meatTotal + this.vegTotal).toFixed(2);
+	}
+}
+
 function getReceipt() {
-	var text1 = "<h3>You Ordered:</h3>";
-	var runningTotal = 0;
-	var sizeTotal = 0;
-	var sizeArray = document.getElementsByClassName("size");
-	for(var i = 0; i < sizeArray.length; i++)
-		if(sizeArray[i].checked) {
-			var selectedSize = sizeArray[i].value;
-			text1 = text1 + selectedSize+"<br>";
-		}
-	if(selectedSize === "Personal Pizza")
-		sizeTotal = 6;
-	else if(selectedSize === "Small Pizza")
-		sizeTotal = 8;
-	else if(selectedSize === "Medium Pizza")
-		sizeTotal = 10;
-	else if(selectedSize === "Large Pizza")
-		sizeTotal = 14;
-	else if(selectedSize === "Extra Large Pizza")
-		sizeTotal = 16;
+	// create order object
+	order = new Order();
 	
-	runningTotal = sizeTotal;
-	console.log(selectedSize+" = $"+sizeTotal+".00");
-	console.log("size text1: "+text1);
-	console.log("subtotal: $"+runningTotal+".00");
-	text1 = getMeat(0, text1);
-	getVeg(runningTotal, text1);
-}
-
-function getMeat(runningTotal, text1) {
-	var meatTotal = 0;
-	var selectedMeat = [];
-	var meatArray = document.getElementsByClassName("meats");
-	for(var j=0;j<meatArray.length; j++)
-		if(meatArray[j].checked) {
-			selectedMeat.push(meatArray[j].value);
-			console.log("selected meat item: ("+meatArray[j].value+")");
-			text1 = text1 +=meatArray[j].value+"<br>";
-		}
-	var meatCount = selectedMeat.length;
-	if(meatCount > 1) meatTotal = (meatCount -1);
-	else meatTotal = 0;
-	runningTotal += meatTotal;
-	console.log("total selected meat items: "+meatCount);
-	console.log(meatCount+" meat - 1 free meat = $"+meatTotal+".00");
-	console.log("meat text1: "+text1);
-	console.log("Purchase Total: $"+runningTotal+".00");
-	return text1;
-}
-
-function getVeg(runningTotal, text1) {
-	var vegTotal = 0;
-	var selectedVeg = [];
-	var vegArray = document.getElementsByClassName("veg");
-	for(var j=0;j<vegArray.length; j++)
-		if(vegArray[j].checked) {
-			selectedVeg.push(vegArray[j].value);
-			console.log("selected veg item: ("+vegArray[j].value+")");
-			text1 = text1 +=vegArray[j].value+"<br>";
-		}
-	vegTotal = selectedVeg.length;
-	// change getVeg function to mimic getMeat()
-	if(vegTotal>0) vegTotal--;
-	runningTotal += vegTotal;
-	console.log("total selected veg items: "+vegTotal);
-	console.log("veg text1: "+text1);
-	console.log("Purchase Total: $"+runningTotal+".00");
-	document.getElementById("showText").innerHTML = text1;
-	document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+
-		runningTotal+".00</strong></h3>";
+	// compute order
+	order.pieSize();
+	order.addMeats();
+	order.addVeggies();
+	
+	// format output string
+	var output = order.size + ": $"+order.sizeTotal.toFixed(2)+"<br /><br />";
+	output += order.meats.length + " meats: $"+order.meatTotal.toFixed(2)+"<br />";
+	output += order.veg.length + " veg: $"+order.vegTotal.toFixed(2)+"<br /><br />";
+	output += "TOTAL: "+order.total();
+	
+	// output order details
+	document.getElementById("output").innerHTML = output;
 }
